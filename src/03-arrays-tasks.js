49bb5f012e5901a4b9316409dac2991ed1705ea5
Olena Honcharuk <gasmin07@gmail.com>
2022-01-08 22:18:46 +0200

    fix: linter

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -557,7 +557,7 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes/*(arr, indexes */) {
+function getElementByIndexes(/* arr, indexes */) {
   throw new Error('Not implemented');
 }
 
